567796170d77bb81fe699f91f348bda1e906267f
Fabaladibbasey <fabaladibbasey27@gmail.com>
2021-07-25 08:01:21 +0000

    magic square is indeed magic square

--- MagicSquare.java
+++ MagicSquare.java
@@ -9,49 +9,47 @@ import java.util.List;
 public class MagicSquare {
 
 	public static void main(String[] args) {
-		List<List<Integer>> matrix = new ArrayList<List<Integer>>();
 		//4 9 2
 		//3 5 7
 		//8 1 5
-		Integer[] arr1 = {3, 9, 2}; 
-		List<Integer> arr1List = new ArrayList<Integer>(Arrays.asList(arr1));
-		matrix.add(arr1List);
-		Integer[] arr2 = {3, 5, 7}; 
-		List<Integer> arr2List = new ArrayList<Integer>(Arrays.asList(arr2));
-		matrix.add(arr2List);
-		Integer[] arr3 = {8, 1, 5}; 
-		List<Integer> arr3List = new ArrayList<Integer>(Arrays.asList(arr3));
-		matrix.add(arr3List);
+		Integer[][] arr = { {4, 9, 2}, {3, 5, 7}, {8, 1, 5} }; 
+		List<List<Integer>> matrix = new ArrayList<List<Integer>>();
+		for (Integer[] list : arr) {
+			matrix.add(Arrays.asList(list));
+		}
 		System.out.println(matrix);
 		System.out.println(formingMagicSquare(matrix));
 	}
 
     public static int formingMagicSquare(List<List<Integer>> s) {
     // Write your code here
-    	int cost = 0;
-    	if(s.get(1).get(1) != 5) {
-    		cost += (s.get(1).get(1) - 5);
-    		s.get(1).set(1, 5);
+    	Integer[][][] pC = {
+    			
+    		   {{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+               {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+               {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+               {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}}, 
+               {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+               {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}}, 
+               {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}}, 
+               {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}},
+    	};
+    	int cost = 1000000; //initial cost set to high cost, infinity if possible
+    	for (int i = 0; i < pC.length; i++) {
+    		List<List<Integer>> newlist = new ArrayList<List<Integer>>(s);
+    		int newCost = 0;
+    		for (int j = 0; j < pC[i].length; j++) {
+    			for (int k = 0; k < pC[i][j].length; k++) {
+    				if(newlist.get(j).get(k) != pC[i][j][k]) {
+    					newCost += Math.abs((newlist.get(j).get(k) -  pC[i][j][k]));
+    				}
+    			}
+    		}
+    		if (newCost < cost) {
+    			cost = newCost;
+    		}
     	}
-    	for (int i = 2; i < 9; i += 2) {
-    		if((s.get(0).get(0) % 2 != 0) && (s.get(0).get(2) != i && s.get(2).get(0) != i && s.get(2).get(2) != i)) {
-    			cost += (s.get(0).get(0) - i);
-    			s.get(0).set(0, i);
-    		}
-    		if((s.get(0).get(2) % 2 != 0) && (s.get(0).get(0) != i && s.get(2).get(0) != i && s.get(2).get(2) != i)) {
-    			cost += (s.get(0).get(2) - i);
-    			s.get(0).set(2, i);
-    		}
-    		if((s.get(2).get(0) % 2 != 0) && (s.get(0).get(0) != i && s.get(0).get(2) != i && s.get(2).get(2) != i)) {
-    			cost += (s.get(2).get(0) - i);
-    			s.get(2).set(0, i);
-    		}
-    		if((s.get(2).get(2) % 2 != 0) && (s.get(0).get(0) != i && s.get(0).get(2) != i && s.get(2).get(0) != i)) {
-    			cost += (s.get(2).get(2) - i);
-    			s.get(2).set(2, i);
-    		}
-    	}
-    	System.out.println(s);
+    	
     	return cost;
     }
 }
