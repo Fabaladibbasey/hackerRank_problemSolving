6ade00f59c9bcb4849f9f3705dda81c52a9c6919
Fabaladibbasey <fabaladibbasey27@gmail.com>
2021-07-28 17:47:10 +0000

    time complexity solved!

--- ClimbingLeaderboard.java
+++ ClimbingLeaderboard.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 //https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
@@ -20,68 +22,74 @@ public class ClimbingLeaderboard {
 		List<Integer> list = new ArrayList<Integer>(Arrays.asList(arr));
 		List<Integer> playerList = new ArrayList<Integer>(Arrays.asList(playerArr));
 		System.out.println(climbingLeaderboard(list, playerList));
+	
 	}
 
-//while this works fine but exceeded the time limit 
-//    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
-//    // player -> Alice
-//    // rank -> Leaderboard ranking
-//    	List<Integer> rankTrack = new ArrayList<Integer>();
-//    	Set<Integer> rankedSet = new HashSet<Integer>(ranked);
-//    	for (int i = 0; i < player.size(); i++) {
-//    		rankTrack.add(scoreIndex(rankedSet, player.get(i)) + 1);
-//    	}
-//    	return rankTrack;
-//    }
-//
-// public static int scoreIndex(Set<Integer> ranks, int score){
-//	 ranks.add(score);
-//	 List<Integer> scoreInsertedList = new ArrayList<Integer>(ranks);
-//	 Collections.sort(scoreInsertedList, Collections.reverseOrder());
-//	 return indexOf(scoreInsertedList, score);
-// 
-// }
-// 
-// public static int indexOf(List<Integer> list, int value) {
-//	 //first occurrence using binary search on descending order list
-//	 int left = 0;
-//	 int right = list.size() - 1;
-//	 while (left <= right) {
-//		 int mid = left + (right - left) / 2;
-//		 if(list.get(mid) == value) {
-//			 return mid;
-//		 }else if(list.get(mid) < value) {
-//			 //search left
-//			 right = mid - 1;
-//		 }else {
-//			 //search right
-//			 left = mid + 1;
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+    // player -> Alice
+    // rank -> Leaderboard ranking
+    	List<Integer> rankTrack = new ArrayList<Integer>();
+    	Map<Integer, Integer> ranking = new LinkedHashMap<Integer, Integer>();
+    	int position = 1;
+    	for (int score : ranked) {
+    		if (!ranking.containsKey(score)) {
+    			ranking.put(score, position);
+    			position++;
+    		}
+    	}
+    	
+    	for (int i = 0; i < player.size(); i++) {
+    		int index = ancestor(ranked, player.get(i));
+    		if (index == -1) {
+    			rankTrack.add(1);
+    		}else {
+    			rankTrack.add(ranking.get(ranked.get(index)) + 1);
+    		}
+    	}
+    	return rankTrack;
+    }
+    
+ public static int ancestor(List<Integer> list, int value) {
+	 //first occurrence using binary search on descending order list
+	 int left = 0;
+	 int right = list.size() - 1;
+	 int index = -1;
+	 while (left <= right) {
+		 int mid = left + (right - left) / 2;
+		 if(list.get(mid) <= value) {
+			 //search left
+			 right = mid - 1;
+		 }else {
+			 //search right
+			 index = mid;
+			 left = mid + 1;
+		 }
+	 }
+	 return index;
+ }
+ 
+//	 public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+//		 List<Integer> rankTrack = new ArrayList<Integer>();
+//		 Set<Integer> set = new HashSet<Integer>(ranked);
+//		 List<Integer> uniqueList = new ArrayList<Integer>(set);
+//		 Collections.sort(uniqueList, Collections.reverseOrder());
+//		 for (int i = 0; i < player.size(); i++) {
+//			 rankTrack.add(position(uniqueList, player.get(i)));
 //		 }
+//		 return rankTrack;
 //	 }
-//	 return -1;
-// }
-	 public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
-		 List<Integer> rankTrack = new ArrayList<Integer>();
-		 Set<Integer> set = new HashSet<Integer>(ranked);
-		 List<Integer> uniqueList = new ArrayList<Integer>(set);
-		 Collections.sort(uniqueList, Collections.reverseOrder());
-		 for (int i = 0; i < player.size(); i++) {
-			 rankTrack.add(position(uniqueList, player.get(i)));
-		 }
-		 return rankTrack;
-	 }
-	 
-	 public static int position(List<Integer> rank, int score) {
-		 int p = 1;
-		 int i = 0;
-		 while (i < rank.size()) {
-			 if(score < rank.get(i)) {
-				 p++;
-			}else {
-				return p;
-			}
-			 i++;
-		 }
-		 return p;
-	 }
+//	 
+//	 public static int position(List<Integer> rank, int score) {
+//		 int p = 1;
+//		 int i = 0;
+//		 while (i < rank.size()) {
+//			 if(score < rank.get(i)) {
+//				 p++;
+//			}else {
+//				return p;
+//			}
+//			 i++;
+//		 }
+//		 return p;
+//	 }
 }
